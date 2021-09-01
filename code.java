7f326146d8628b757c33fa915623ada0b29339d9
Gowtham <56188220+Gowtham-GitHub@users.noreply.github.com>
2021-09-01 23:26:23 +0530

    changed printIn to printf

--- code.java
+++ code.java
@@ -1,6 +1,6 @@
 class Simple{
   public static void main(String args[]) throws InterruptedException{
     Thread.sleep(5000);
-    System.out.printIn("Hello World by Java");
+    System.out.printf("Hello World by Java");
   }
 }
